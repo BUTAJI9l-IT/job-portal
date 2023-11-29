c197f7c8f571406fbf5a550cfd9bf8ed4c2e35db
vitalii.bortsov <bortsov@seznam.cz>
2023-11-29 23:41:37 +0100

    chore: equalsAndHashCode in dto

--- job-portal-be/src/main/java/muni/fi/cz/jobportal/api/search/OccupationQueryParams.java
+++ job-portal-be/src/main/java/muni/fi/cz/jobportal/api/search/OccupationQueryParams.java
@@ -2,10 +2,12 @@
 package muni.fi.cz.jobportal.api.search;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
 import static muni.fi.cz.jobportal.configuration.constants.SearchProperties.NAME;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 public class OccupationQueryParams extends QueryParams {
